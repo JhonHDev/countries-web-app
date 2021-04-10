07332793e05e94bc8d20633dfbaa3124f46970e2
Jhon Esteban Herrera <estebanhz9919@gmail.com>
2021-04-09 19:16:54 -0500

    [Add]: Initial structure of the project

---
+++ src/containers/CountriesApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../assets/styles/global.scss';
+
+export const CountriesApp = () => {
+  return (
+    <div>
+      <h1>CountriesApp</h1>
+    </div>
+  );
+};
